655fc64cbd44cc88a64f064585feb270808121bc
ermcgonagle <ermcgonagle>
2024-01-27 17:51:52 -0600

    initial home page styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import React from 'react';
+import test from './test.js';
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to CodeHoot</h1>
+      <h1>Welcome to Codehoot!</h1>
+      <div className="probStatement">Click start whenever everyone is ready. Who doth comeout triumphant over their peers?</div>
+
+      <div className='container'>
+        <input
+          type="text"
+          className="username-input"
+          placeholder="username"
+        />
+        <button className='startButton'>Let's Code!</button>
+      </div>
     </div>
   );
 }
