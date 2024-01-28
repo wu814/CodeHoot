d1e9c9aca426d715578845b6f460c9258ff6523f
wu814 <william14.wu@gmail.com>
2024-01-27 23:07:43 -0600

    nicer scoreboard

--- src/components/UserRank.js
+++ src/components/UserRank.js
@@ -2,12 +2,17 @@ import React, {useState} from 'react'
 import "./css/UserRank.css"
 
 const UserRank = ({ userName, totalTime }) => {
-    const [time, setTime] = useState();
-    return (
-        <div className='userRankRow'>
-            {userName} : {totalTime}
-        </div>
-    )
+  const [time, setTime] = useState();
+  return (
+    <div className='userRankRow'>
+      <div className='name'>
+        {userName}
+      </div>
+      <div className='time'>
+        {totalTime}
+      </div>
+    </div>
+  )
 }
 
 export default UserRank
