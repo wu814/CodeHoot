a0743c43d5325b39d810f2803c4d60f7c3d8ceaf
Andrew Lin <andrew.mlin27@gmail.com>
2024-01-27 18:23:00 -0600

    fixing navbar

--- src/App.js
+++ src/App.js
@@ -9,18 +9,8 @@ import Scoreboard from "./components/Scoreboard"
 function App() {
   return (
     <div className="App">
-      <h1>Welcome to Codehoot!</h1>
-      <div className="probStatement">Click start whenever everyone is ready. Who doth comeout triumphant over their peers?</div>
-
-      <div className='container'>
-        <input
-          type="text"
-          className="username-input"
-          placeholder="username"
-        />
-        <button className='startButton'>Let's Code!</button>
-      </div>
-      {/* <Router>
+      <>
+        <Navbar />
         <div>
           <Routes>
             <Route path="/" component={Home} />
@@ -29,8 +19,9 @@ function App() {
             <Route path="/scoreboard" component={Scoreboard} />
           </Routes>
         </div>
-      </Router> */}
-      
+      </>
+      <h1>Welcome to CodeHoot</h1>
+    
     </div>
   );
 }
