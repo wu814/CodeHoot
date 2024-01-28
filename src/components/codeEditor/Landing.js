0a8547d63ecc765f71f8ae7b2af08b0607643755
Daniel Querrey <daniel.q@tamu.edu>
2024-01-28 03:04:10 -0600

    basic overlay and format. need to implement timer and kahoot colors

--- src/components/codeEditor/Landing.js
+++ src/components/codeEditor/Landing.js
@@ -16,6 +16,8 @@ import OutputDetails from "./OutputDetails";
 import ThemeDropdown from "./ThemeDropdown";
 import LanguagesDropdown from "./LanguagesDropdown";
 
+import "./css/Landing.css";
+
 const javascriptDefault = `/**
 * Problem: Binary Search: Search a sorted array for a target value.
 */
@@ -46,218 +48,213 @@ const target = 5;
 console.log(binarySearch(arr, target));
 `;
 
+const problemDefault = `/** This is where we load the problem **/`;
+
 const Landing = () => {
-  const [code, setCode] = useState(javascriptDefault);
-  const [customInput, setCustomInput] = useState("");
-  const [outputDetails, setOutputDetails] = useState(null);
-  const [processing, setProcessing] = useState(null);
-  const [theme, setTheme] = useState("cobalt");
-  const [language, setLanguage] = useState(languageOptions[0]);
+    const [code, setCode] = useState(javascriptDefault);
+    const [problem, setProblem] = useState(problemDefault);
 
-  const enterPress = useKeyPress("Enter");
-  const ctrlPress = useKeyPress("Control");
+    const [customInput, setCustomInput] = useState("");
+    const [outputDetails, setOutputDetails] = useState(null);
+    const [processing, setProcessing] = useState(null);
+    const [theme, setTheme] = useState("cobalt");
+    const [language, setLanguage] = useState(languageOptions[0]);
 
-  const onSelectChange = (sl) => {
-    console.log("selected Option...", sl);
-    setLanguage(sl);
-  };
+    const enterPress = useKeyPress("Enter");
+    const ctrlPress = useKeyPress("Control");
 
-  useEffect(() => {
-    if (enterPress && ctrlPress) {
-      console.log("enterPress", enterPress);
-      console.log("ctrlPress", ctrlPress);
-      handleCompile();
-    }
-  }, [ctrlPress, enterPress]);
-  const onChange = (action, data) => {
-    switch (action) {
-      case "code": {
-        setCode(data);
-        break;
-      }
-      default: {
-        console.warn("case not handled!", action, data);
-      }
-    }
-  };
-  const handleCompile = () => {
-    setProcessing(true);
-    const formData = {
-      language_id: language.id,
-      // encode source code in base64
-      source_code: btoa(code),
-      stdin: btoa(customInput),
-    };
-    const options = {
-      method: "POST",
-      url: process.env.REACT_APP_RAPID_API_URL,
-      params: { base64_encoded: "true", fields: "*" },
-      headers: {
-        "content-type": "application/json",
-        "Content-Type": "application/json",
-        "X-RapidAPI-Host": process.env.REACT_APP_RAPID_API_HOST,
-        "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
-      },
-      data: formData,
+    const onSelectChange = (sl) => {
+        console.log("selected Option...", sl);
+        setLanguage(sl);
     };
 
-    axios
-      .request(options)
-      .then(function (response) {
-        console.log("res.data", response.data);
-        const token = response.data.token;
-        checkStatus(token);
-      })
-      .catch((err) => {
-        let error = err.response ? err.response.data : err;
-        // get error status
-        let status = err.response.status;
-        console.log("status", status);
-        if (status === 429) {
-          console.log("too many requests", status);
-
-          showErrorToast(
-            `Quota of 100 requests exceeded for the Day! Please read the blog on freeCodeCamp to learn how to setup your own RAPID API Judge0!`,
-            10000
-          );
+    useEffect(() => {
+        if (enterPress && ctrlPress) {
+            console.log("enterPress", enterPress);
+            console.log("ctrlPress", ctrlPress);
+            handleCompile();
         }
-        setProcessing(false);
-        console.log("catch block...", error);
-      });
-  };
-
-  const checkStatus = async (token) => {
-    const options = {
-      method: "GET",
-      url: process.env.REACT_APP_RAPID_API_URL + "/" + token,
-      params: { base64_encoded: "true", fields: "*" },
-      headers: {
-        "X-RapidAPI-Host": process.env.REACT_APP_RAPID_API_HOST,
-        "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
-      },
+    }, [ctrlPress, enterPress]);
+    const onChange = (action, data) => {
+        switch (action) {
+            case "code": {
+                setCode(data);
+                break;
+            }
+            default: {
+                console.warn("case not handled!", action, data);
+            }
+        }
     };
-    try {
-      let response = await axios.request(options);
-      let statusId = response.data.status?.id;
+    const handleCompile = () => {
+        setProcessing(true);
+        const formData = {
+            language_id: language.id,
+            // encode source code in base64
+            source_code: btoa(code),
+            stdin: btoa(customInput),
+        };
+        const options = {
+            method: "POST",
+            url: process.env.REACT_APP_RAPID_API_URL,
+            params: { base64_encoded: "true", fields: "*" },
+            headers: {
+                "content-type": "application/json",
+                "Content-Type": "application/json",
+                "X-RapidAPI-Host": process.env.REACT_APP_RAPID_API_HOST,
+                "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+            },
+            data: formData,
+        };
 
-      // Processed - we have a result
-      if (statusId === 1 || statusId === 2) {
-        // still processing
-        setTimeout(() => {
-          checkStatus(token);
-        }, 2000);
-        return;
-      } else {
-        setProcessing(false);
-        setOutputDetails(response.data);
-        showSuccessToast(`Compiled Successfully!`);
-        console.log("response.data", response.data);
-        return;
-      }
-    } catch (err) {
-      console.log("err", err);
-      setProcessing(false);
-      showErrorToast();
+        axios
+            .request(options)
+            .then(function (response) {
+                console.log("res.data", response.data);
+                const token = response.data.token;
+                checkStatus(token);
+            })
+            .catch((err) => {
+                let error = err.response ? err.response.data : err;
+                // get error status
+                let status = err.response.status;
+                console.log("status", status);
+                if (status === 429) {
+                    console.log("too many requests", status);
+
+                    showErrorToast(`Quota of 100 requests exceeded for the Day! Please read the blog on freeCodeCamp to learn how to setup your own RAPID API Judge0!`, 10000);
+                }
+                setProcessing(false);
+                console.log("catch block...", error);
+            });
+    };
+
+    const checkStatus = async (token) => {
+        const options = {
+            method: "GET",
+            url: process.env.REACT_APP_RAPID_API_URL + "/" + token,
+            params: { base64_encoded: "true", fields: "*" },
+            headers: {
+                "X-RapidAPI-Host": process.env.REACT_APP_RAPID_API_HOST,
+                "X-RapidAPI-Key": process.env.REACT_APP_RAPID_API_KEY,
+            },
+        };
+        try {
+            let response = await axios.request(options);
+            let statusId = response.data.status?.id;
+
+            // Processed - we have a result
+            if (statusId === 1 || statusId === 2) {
+                // still processing
+                setTimeout(() => {
+                    checkStatus(token);
+                }, 2000);
+                return;
+            } else {
+                setProcessing(false);
+                setOutputDetails(response.data);
+                showSuccessToast(`Compiled Successfully!`);
+                console.log("response.data", response.data);
+                return;
+            }
+        } catch (err) {
+            console.log("err", err);
+            setProcessing(false);
+            showErrorToast();
+        }
+    };
+
+    function handleThemeChange(th) {
+        const theme = th;
+        console.log("theme...", theme);
+
+        if (["light", "vs-dark"].includes(theme.value)) {
+            setTheme(theme);
+        } else {
+            defineTheme(theme.value).then((_) => setTheme(theme));
+        }
     }
-  };
+    useEffect(() => {
+        defineTheme("oceanic-next").then((_) => setTheme({ value: "oceanic-next", label: "Oceanic Next" }));
+    }, []);
 
-  function handleThemeChange(th) {
-    const theme = th;
-    console.log("theme...", theme);
+    const showSuccessToast = (msg) => {
+        toast.success(msg || `Compiled Successfully!`, {
+            position: "top-right",
+            autoClose: 1000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+        });
+    };
+    const showErrorToast = (msg, timer) => {
+        toast.error(msg || `Something went wrong! Please try again.`, {
+            position: "top-right",
+            autoClose: timer ? timer : 1000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+        });
+    };
 
-    if (["light", "vs-dark"].includes(theme.value)) {
-      setTheme(theme);
-    } else {
-      defineTheme(theme.value).then((_) => setTheme(theme));
-    }
-  }
-  useEffect(() => {
-    defineTheme("oceanic-next").then((_) =>
-      setTheme({ value: "oceanic-next", label: "Oceanic Next" })
+    return (
+        <>
+            {/* <ToastContainer position='top-right' autoClose={2000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover /> */}
+            {/* <div className='h-4 bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500'></div> */}
+            <div className='question-container'>
+                <div className='top-container'>
+                    <div className='left-panel'>
+                        <div className='px-4 py-2' id='select-language'>
+                            <LanguagesDropdown onSelectChange={onSelectChange} />
+                        </div>
+                        <div className='px-4 py-2' id='theme-change'>
+                            <ThemeDropdown handleThemeChange={handleThemeChange} theme={theme} />
+                        </div>
+                    </div>
+                    <div className='top-problem-statement'>
+                        <CodeEditorWindow code={problem} onChange={onChange} language={language?.value} theme={theme.value} />
+                    </div>
+                    <div className='right-top-panel'>
+                        <p>Right Panel</p>
+                    </div>
+                </div>
+
+                <div className='btm-container'>
+                    <div className='problem-statement'>
+                        <div id='editor-window'>
+                            <CodeEditorWindow code={code} onChange={onChange} language={language?.value} theme={theme.value} />
+                        </div>
+                    </div>
+                    <div className='right-panel'>
+                        <div id='output-window'>
+                            <OutputWindow outputDetails={outputDetails}  />
+                        </div>
+                        <div id='custom-input'>
+                            <CustomInput customInput={customInput} setCustomInput={setCustomInput} />
+                        </div>
+                        <div id='custom-compile'>
+                            <button
+                                onClick={handleCompile}
+                                disabled={!code}
+                                className={classnames(
+                                    "mt-4 border-2 border-black z-10 rounded-md shadow-[5px_5px_0px_0px_rgba(0,0,0)] px-4 py-2 hover:shadow transition duration-200 bg-white flex-shrink-0",
+                                    !code ? "opacity-50" : ""
+                                )}
+                            >
+                                {processing ? "Processing..." : "Compile and Execute"}
+                            </button>
+                        </div>
+                    </div>
+                    <div className='far-right-panel'>
+                        <div id='output-details'>{outputDetails && <OutputDetails outputDetails={outputDetails} />}</div>
+                    </div>
+                </div>
+            </div>
+        </>
     );
-  }, []);
-
-  const showSuccessToast = (msg) => {
-    toast.success(msg || `Compiled Successfully!`, {
-      position: "top-right",
-      autoClose: 1000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-    });
-  };
-  const showErrorToast = (msg, timer) => {
-    toast.error(msg || `Something went wrong! Please try again.`, {
-      position: "top-right",
-      autoClose: timer ? timer : 1000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-    });
-  };
-
-  return (
-    <>
-      <ToastContainer
-        position="top-right"
-        autoClose={2000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-      />
-
-      
-
-      <div className="h-4 w-full bg-gradient-to-r from-pink-500 via-red-500 to-yellow-500"></div>
-      <div className="flex flex-row">
-        <div className="px-4 py-2">
-          <LanguagesDropdown onSelectChange={onSelectChange} />
-        </div>
-        <div className="px-4 py-2">
-          <ThemeDropdown handleThemeChange={handleThemeChange} theme={theme} />
-        </div>
-      </div>
-      <div className="flex flex-row space-x-4 items-start px-4 py-4">
-        <div className="flex flex-col w-full h-full justify-start items-end">
-          <CodeEditorWindow
-            code={code}
-            onChange={onChange}
-            language={language?.value}
-            theme={theme.value}
-          />
-        </div>
-
-        <div className="right-container flex flex-shrink-0 w-[30%] flex-col">
-          <OutputWindow outputDetails={outputDetails} />
-          <div className="flex flex-col items-end">
-            <CustomInput
-              customInput={customInput}
-              setCustomInput={setCustomInput}
-            />
-            <button
-              onClick={handleCompile}
-              disabled={!code}
-              className={classnames(
-                "mt-4 border-2 border-black z-10 rounded-md shadow-[5px_5px_0px_0px_rgba(0,0,0)] px-4 py-2 hover:shadow transition duration-200 bg-white flex-shrink-0",
-                !code ? "opacity-50" : ""
-              )}
-            >
-              {processing ? "Processing..." : "Compile and Execute"}
-            </button>
-          </div>
-          {outputDetails && <OutputDetails outputDetails={outputDetails} />}
-        </div>
-      </div>
-      {/* <Footer /> */}
-    </>
-  );
 };
 export default Landing;
