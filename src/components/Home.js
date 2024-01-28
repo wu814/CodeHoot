423519d80375203d5e0815acb19343ea48618687
Andrew Lin <andrew.mlin27@gmail.com>
2024-01-28 04:33:44 -0600

    getDoc

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,26 +2,41 @@ import React , { useRef } from 'react'
 import "./css/Home.css"
 import { Link } from 'react-router-dom'
 import { firestore } from "../firebase_setup/firebase"
-import { addDoc, collection } from "@firebase/firestore"
+import { addDoc, collection, getDocs } from "@firebase/firestore"
 
 
 const Home = () => {
 
   const messageRef = useRef()
-  const ref = collection(firestore, "messages")
+  const ref = collection(firestore, "names")
 
   const handleSubmit = (e) => {
     e.preventDefault()
     console.log(messageRef.current.value)
-
+    
     let data = {
-      message: parseInt(messageRef.current.value)+1
+      name: messageRef.current.value,
+      score: 0
     }
 
     try {
       addDoc(ref, data)
     } catch(err) {
       console.log(err)
+    }
+
+    messageRef.current.value = ""
+  }
+
+  const fetchData = async () => {
+    try {
+      const querySnapshot = await getDocs(ref);
+      querySnapshot.forEach((doc) => {
+        console.log(doc.id, ' => ', doc.data()['name'], ' => ', doc.data()['score']);
+        // Process each document here (e.g., update state with the fetched data)
+      });
+    } catch (error) {
+      console.error('Error getting documents: ', error);
     }
   }
 
@@ -43,6 +58,8 @@ const Home = () => {
         <input type="text" ref={messageRef} />
         <button type="submit">Send</button>
       </form>
+
+      <button onClick={fetchData}>Fetch</button>
 
     </div>
   )
