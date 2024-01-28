017580352bb9e59bc0531ab972d40f4199762577
ermcgonagle <ermcgonagle>
2024-01-27 18:24:09 -0600

    storing minor changes

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import { useHistory } from "react-router-dom"
 import "./css/Home.css"
 
 const Home = () => {
+  const history = useHistory();
+
+
+  const handleStartClick = () => {
+    history.push("/test");
+  }
   return (
       <div className="Home">
         <h1>Welcome to Codehoot!</h1>
@@ -13,7 +20,7 @@ const Home = () => {
             className="username-input"
             placeholder="username"
           />
-          <button className='startButton'>Let's Code!</button>
+          <button className='startButton' onClick={handleStartClick}>Let's Code!</button>
         </div>
       </div>
   )
