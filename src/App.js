3d5a16493df874ecb09e96477541b2875d694f84
Daniel Querrey <daniel.q@tamu.edu>
2024-01-27 18:47:52 -0600

    New BP w/Code Editor

--- src/App.js
+++ src/App.js
@@ -1,29 +1,8 @@
-import './App.css';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
-import Navbar from "./components/Navbar"
-import Home from "./components/Home"
-import Lobby from "./components/Lobby"
-import Question from "./components/Question"
-import Scoreboard from "./components/Scoreboard"
+import "./App.css";
+import Landing from "./components/codeEditor/Landing";
 
 function App() {
-  return (
-    <div className="App">
-      <>
-        <Navbar />
-        <div>
-          <Routes>
-            <Route path="/" element={<Home/>} />
-            <Route path="/lobby" element={<Lobby/>} />
-            <Route path="/question" element={<Question/>} />
-            <Route path="/scoreboard" element={<Scoreboard/>} />
-          </Routes>
-        </div>
-      </>
-      <h1>Welcome to CodeHoot</h1>
-
-    </div>
-  );
+  return <Landing />;
 }
 
 export default App;
