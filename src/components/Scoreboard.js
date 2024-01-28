52993cef7e5cc92817b60fe844864dfaf668c405
wu814 <william14.wu@gmail.com>
2024-01-28 08:50:13 -0600

    scoreboard working

--- src/components/Scoreboard.js
+++ src/components/Scoreboard.js
@@ -1,22 +1,50 @@
-import React from 'react'
+"use client"; // necessary for useState to work
+import React, {useState, useEffect} from 'react'
 import "./css/Scoreboard.css"
 import UserRank from './UserRank'
+import {getDocs, collection} from "@firebase/firestore"
+import { firestore } from "../firebase_setup/firebase"
+
 
 const Scoreboard = () => {
+
+  const [playerList, setPlayerList] = useState([]) 
+  const [sortedPlayerList, setSortedPlayerList] = useState([])
+  
+  const sortPlayerList = (playerList) => {
+      setSortedPlayerList(playerList.sort((a, b) => 
+      b.props.totalScore - a.props.totalScore
+    ))
+  }
+  const fetchData = async () => {
+    setPlayerList([])
+    setSortedPlayerList([])
+    const ref = collection(firestore, "names")
+    try {
+      const querySnapshot = await getDocs(ref);
+      querySnapshot.forEach((player) => {
+        setPlayerList(prevList => [...prevList, <UserRank userName={player.data()['name']} totalScore={player.data()['score']}/>])
+      }); 
+    } catch (error) {
+      console.error('Error getting documents: ', error);
+    }    
+  }
+  useEffect(() => {
+    sortPlayerList(playerList)
+  }, [playerList])
+
   return (
     <div className='main'>
 
       <div className='header'>
         <h1>Scoreboard</h1> 
       </div>
-
+      <button onClick={fetchData}>reload</button>
       <div className='leaderBoard'>
         <ul className='playerList'> 
-          <li><UserRank userName={"Shannon"} totalTime={69} /></li>
-          <li><UserRank userName={"Andrew"} totalTime={79} /></li>
-          <li><UserRank userName={"Daniel"} totalTime={89} /></li>
-          <li><UserRank userName={"Wu"} totalTime={99} /></li>
-          <li><UserRank userName={"Jack"} totalTime={99} /></li>
+          {sortedPlayerList.slice(0,5).map((player, index) => (
+            <li key={index}>{player}</li>
+          ))}
         </ul>
       </div>
     </div>
