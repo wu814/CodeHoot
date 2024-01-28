43c281ce7a27e6a4434830b679ed545ec633f23c
Andrew Lin <andrew.mlin27@gmail.com>
2024-01-27 18:00:40 -0600

    navbar working

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router, Route } from "react-router-dom"
+import { Route, Routes } from "react-router-dom"
 import Navbar from "./components/Navbar"
 import Home from "./components/Home"
 import Lobby from "./components/Lobby"
@@ -9,15 +9,17 @@ import Scoreboard from "./components/Scoreboard"
 function App() {
   return (
     <div className="App">
-      {/* <Router>
+      <>
+        <Navbar />
         <div>
-          <Navbar />
-          <Route exact path="/" component={Home} />
-          <Route path="/lobby" component={Lobby} />
-          <Route path="/question" component={Question} />
-          <Route path="/scoreboard" component={Scoreboard} />
+          <Routes>
+            <Route path="/" component={Home} />
+            <Route path="/lobby" component={Lobby} />
+            <Route path="/question" component={Question} />
+            <Route path="/scoreboard" component={Scoreboard} />
+          </Routes>
         </div>
-      </Router> */}
+      </>
       <h1>Welcome to CodeHoot</h1>
       
     </div>
